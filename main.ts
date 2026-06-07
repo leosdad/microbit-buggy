7e293f95b06cb612f23b18c9d5bb80e310441fb5
leosdad <pechansky@hypervisual.com.br>
2026-06-07 16:03:03 -0300

    More tests

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
 
+/*
+
+Avoid these for the external LED if you want to stay away from board conflicts:
+
+P3, P4, P6, P7, P9, P10: shared with the LED matrix
+P12: reserved
+P19, P20: used by the onboard I2C sensors
+
+*/
+
 // Servo functions
 
 function goForward() {
@@ -19,7 +29,9 @@ function stop() {
 // Main program
 
 goForward()
+pins.digitalWritePin(DigitalPin.P8, 1) // Turn on LED
 basic.pause(1000)
 goBackward()
+pins.digitalWritePin(DigitalPin.P8, 0) // Turn off LED
 basic.pause(1000)
 stop()
