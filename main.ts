f29a13381eeba8426d672bb0ab52f2c2b6dbc285
leosdad <pechansky@hypervisual.com.br>
2026-06-07 15:49:27 -0300

    More tests

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
-basic.showLeds(`
-	. # # # .
-	. # . . #
-	. # # # .
-	. # . . #
-	. # # # .
-`)
 
+// Servo functions
+
+function goForward() {
+	pins.servoWritePin(AnalogPin.P0, 180)
+	pins.servoWritePin(AnalogPin.P1, 0)
+}
+
+function goBackward() {
+	pins.servoWritePin(AnalogPin.P0, 0)
+	pins.servoWritePin(AnalogPin.P1, 180)
+}
+
+function stop() {
+	pins.digitalWritePin(DigitalPin.P0, 0)
+	pins.digitalWritePin(DigitalPin.P1, 0)
+}
+
+// Main program
+
+goForward()
+basic.pause(1000)
+goBackward()
+basic.pause(1000)
+stop()
