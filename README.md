7e564c343172e29e5db5d57e19039f8d2e0c8b4a
leosdad <pechansky@hypervisual.com.br>
2026-06-23 16:29:23 -0300

    More tests

--- README.md
+++ README.md
@@ -1,24 +1,55 @@
+# Microbit Buggy
 
-> Open this page at [https://.github.io//](https://.github.io//)
+This project drives a micro:bit v1 buggy over Bluetooth UART using the micro:bit Bluetooth Gamepad web app.
 
-## Use as Extension
+## Wiring
 
-This repository can be added as an **extension** in MakeCode.
+- Left servo signal: P0
+- Right servo signal: P1
+- External LED: P8
+- Avoid P3, P4, P6, P7, P9, and P10 because they are shared with the LED matrix.
 
-* open []()
-* click on **New Project**
-* click on **Extensions** under the gearwheel menu
-* search for **https://github.com/** and import
+## Gamepad Commands
 
-## Edit this project
+The web app sends these UART messages:
 
-To edit this repository in MakeCode.
+- `L` = rotate left while held
+- `R` = rotate right while held
+- `A` = toggle forward / stop
+- `S` = emergency stop
+- `B` = toggle lights on/off
 
-* open []()
-* click on **Import** then click on **Import URL**
-* paste **https://github.com/** and click import
+The buggy has a short timeout so rotation stops shortly after you release the left or right button.
 
-#### Metadata (used for search, rendering)
+## Build And Flash
 
-* for PXT/
-<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+From VS Code in this folder:
+
+```powershell
+npm run build
+npm run flash
+```
+
+## Pairing And Control
+
+1. Flash the code to the micro:bit over USB.
+2. Open the micro:bit Bluetooth Gamepad web app on your phone.
+3. Tap **Connect** and choose your micro:bit.
+4. You do not upload anything to the phone.
+5. Use the gamepad buttons; the app sends the UART commands above automatically.
+
+## What To Do On The Phone
+
+1. Turn Bluetooth on.
+2. Open the micro:bit Bluetooth Gamepad web app.
+3. Press **Connect**.
+4. Select your micro:bit.
+5. Drive with the on-screen buttons.
+
+You still only flash code to the micro:bit. The phone just acts as the controller.
+
+## Notes
+
+- This project uses Bluetooth, not `radio`.
+- If the Bluetooth connection drops, the code turns the LED off and stops both servos.
+- `stop()` cuts the PWM signal entirely to avoid servo buzzing.
--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
 
-/*
+let lastCommandAt = 0
+let forwardEnabled = false
+let rotationMode = "none"
+let lightsEnabled = false
 
-Avoid these for the external LED if you want to stay away from board conflicts:
+const COMMAND_TIMEOUT_MS = 180
 
-P3, P4, P6, P7, P9, P10: shared with the LED matrix
-P12: reserved
-P19, P20: used by the onboard I2C sensors
-
-*/
-
-// Servo functions
+// #region Functions
 
 function goForward() {
 	pins.servoWritePin(AnalogPin.P0, 180)
@@ -21,17 +18,112 @@ function goBackward() {
 	pins.servoWritePin(AnalogPin.P1, 180)
 }
 
+function turnLeft() {
+	pins.servoWritePin(AnalogPin.P0, 90)
+	pins.servoWritePin(AnalogPin.P1, 0)
+}
+
+function turnRight() {
+	pins.servoWritePin(AnalogPin.P0, 90)
+	pins.servoWritePin(AnalogPin.P1, 180)
+}
+
 function stop() {
 	pins.digitalWritePin(DigitalPin.P0, 0)
 	pins.digitalWritePin(DigitalPin.P1, 0)
 }
 
-// Main program
+function turnOnLights() {
+	pins.digitalWritePin(DigitalPin.P8, 1)
+}
 
-goForward()
-pins.digitalWritePin(DigitalPin.P8, 1) // Turn on LED
-basic.pause(1000)
-goBackward()
-pins.digitalWritePin(DigitalPin.P8, 0) // Turn off LED
-basic.pause(1000)
-stop()
+function turnOffLights() {
+	pins.digitalWritePin(DigitalPin.P8, 0)
+}
+
+function setLightsEnabled(enabled: boolean) {
+	lightsEnabled = enabled
+	if (lightsEnabled) {
+		turnOnLights()
+	} else {
+		turnOffLights()
+	}
+}
+
+function stopAll() {
+	stop()
+	setLightsEnabled(false)
+	forwardEnabled = false
+	rotationMode = "none"
+}
+
+function applyCurrentMotion() {
+	if (rotationMode == "left") {
+		turnLeft()
+	} else if (rotationMode == "right") {
+		turnRight()
+	} else if (forwardEnabled) {
+		goForward()
+	} else {
+		stop()
+	}
+}
+
+function markCommandReceived() {
+	lastCommandAt = input.runningTime()
+}
+
+function handleCommand(command: string) {
+	let normalized = command.trim()
+	if (normalized.length == 0) {
+		return
+	}
+
+	markCommandReceived()
+
+	if (normalized == "L") {
+		rotationMode = "left"
+		applyCurrentMotion()
+	} else if (normalized == "R") {
+		rotationMode = "right"
+		applyCurrentMotion()
+	} else if (normalized == "S") {
+		stopAll()
+	} else if (normalized == "A") {
+		forwardEnabled = !forwardEnabled
+		applyCurrentMotion()
+	} else if (normalized == "B") {
+		setLightsEnabled(!lightsEnabled)
+	} else if (normalized == "C") {
+		rotationMode = "none"
+		applyCurrentMotion()
+	}
+}
+
+// #endregion
+
+bluetooth.startUartService()
+
+bluetooth.onBluetoothConnected(function () {
+	markCommandReceived()
+	stopAll()
+})
+
+bluetooth.onBluetoothDisconnected(function () {
+	stopAll()
+})
+
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+	handleCommand(bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)))
+})
+
+basic.forever(function () {
+	if (rotationMode != "none" && input.runningTime() - lastCommandAt > COMMAND_TIMEOUT_MS) {
+		rotationMode = "none"
+		applyCurrentMotion()
+	}
+	basic.pause(100)
+})
+
+stopAll()
+markCommandReceived()
